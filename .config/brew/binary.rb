ebf3bebbe0d1b7239eb0c3c6a5234c1585906951
Megabyte Labs <help@megabyte.space>
2022-04-08 08:04:21 -0400

    ⤵️ chore(synchronize) Applying changes from upstream repository.

--- .config/brew/binary.rb
+++ .config/brew/binary.rb
@@ -1,19 +1,19 @@
-class CommonShared < Formula
-  desc "Includes common files used across different types of common file repositories (e.g. Go, Python, TypeScript, Go, more..)"
+class Probundle < Formula
+  desc "A tool that handles compressing, bundling, and unpacking production node_modules dependencies that you want to store in source control"
   homepage "https://megabyte.space"
-  url "https://github.com/ProfessorManhattan/common-shared/releases/download/v0.0.1/common-shared.tar.gz"
-  version "0.0.1"
+  url "https://github.com/megabyte-labs/Probundle/releases/download/v3.10.3/probundle.tar.gz"
+  version "3.10.3"
   license "MIT"
 
-
+  
 
   def install
     os = OS.kernel_name.downcase
     arch = Hardware::CPU.intel? ? "amd64" : Hardware::CPU.arch.to_s
-    bin.install "build/bin/common-shared-#{os}_#{arch}" => "common-shared"
+    bin.install "build/bin/probundle-#{os}_#{arch}" => "probundle"
   done
 
   test do
-    system bin/"common-shared", "--version"
+    system bin/"probundle", "--version"
   end
 end
